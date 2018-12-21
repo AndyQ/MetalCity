0a9cfeac067e0448fed8e3f0ee1e13e6c2dcf9e2
AndyQ <andy.qua@gmail.com>
2018-12-21 17:44:53 +0000

    Buildings can now have logos! Refactored Modle so that we can have multiple renderpipelines per object if we want (buildings need it as the shader for building and logo is different)

---
+++ Shared/Utils/Font.swift
@@ -0,0 +1,13 @@
+//
+//  Font.swift
+//  MetalCity
+//
+//  Created by Andy Qua on 21/12/2018.
+//  Copyright © 2018 Andy Qua. All rights reserved.
+//
+
+import UIKit
+
+class Font: NSObject {
+
+}
